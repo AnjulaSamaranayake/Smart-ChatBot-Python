5acd0c4baac00cb9d0a1f815f3daabba2d2518e4
AnjulaSamaranayake <anjulasankalpa2255@gmail.com>
2025-03-12 10:00:15 +0530

    did some corrections and did the chatbot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Import the libraries
+# Import the libraries
 from newspaper import Article
 import random
 import string
@@ -10,55 +10,51 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-#Download the punkt package
+# Download the punkt package
 nltk.download('punkt', quiet=True)
 
-#Get the Article
+# Get the Article
 article = Article('https://www.mayoclinic.org/diseases-conditions/chronic-kidney-disease/symptoms-causes/syc-20354521')
 article.download()
 article.parse()
 article.nlp()
 corpus = article.text
 
-#print the article text
-print(corpus)
-
-#Tokenization
+# Tokenization
 text = corpus
-sentences_list = nltk.sent_tokenize(text) # A list of sentences
+sentences_list = nltk.sent_tokenize(text)  # A list of sentences
 
-#Print the list of sentences
-print(sentences_list)
-
-#A function to return a random greeting to a users greeting
+# A function to return a random greeting to a user's greeting
 def greeting_response(text):
     text = text.lower()
 
-    #Users Greeting
+    # Users Greeting
     user_greetings = ['hi', 'hey', 'hello', 'hola']
-    #Bots greeting response
+    # Bots Greeting Response
     bot_greetings = ['hi', 'hey', 'hello']
 
-    for word in text.splot():
+    for word in text.split():
         if word in user_greetings:
             return random.choice(bot_greetings)
+    return None
 
+# Function to sort indexes based on values in descending order
 def index_sort(list_var):
     length = len(list_var)
     list_index = list(range(0, length))
 
     x = list_var
     for i in range(length):
-        for j in range(length):
-            if x[list_index[i]] > x[list_index[j]]:
-                #Swap
+        for j in range(i + 1, length):
+            if x[list_index[i]] < x[list_index[j]]:  # Fix comparison for descending order
+                # Swap
                 temp = list_index[i]
                 list_index[i] = list_index[j]
                 list_index[j] = temp
 
-                return list_index
+    return list_index  # Move outside of the loop
 
-#Create the bots response
+# Create the bot's response
 def bot_response(user_input):
     user_input = user_input.lower()
     sentences_list.append(user_input)
@@ -70,17 +66,31 @@ def bot_response(user_input):
     index = index[1:]
     response_flag = 0
 
-    j=0
+    j = 0
     for i in range(len(index)):
         if similarity_scores_list[index[i]] > 0.0:
-            bot_response = bot_response +''+ sentences_list[index[i]]
+            bot_response += ' ' + sentences_list[index[i]]
             response_flag = 1
-            j=j+1
+            j += 1
             if j > 2:
                 break
 
     if response_flag == 0:
-        bot_response = bot_response+''+"I'm sorry, I don't understand you."
+        bot_response = "I'm sorry, I don't understand you."
 
-        sentences_list.remove(user_input)
-        return bot_response
+    sentences_list.pop()  # Ensure cleanup of user input
+    return bot_response
+
+# Start the chat
+print('Doc Bot: I am Doc Bot. I will answer your questions about Kidney Disease.')
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ['exit', 'bye', 'quit', 'okay bye']:
+        print("Doc Bot: Goodbye! Take care.")
+        break
+    response = greeting_response(user_input)
+    if response:
+        print(f"Doc Bot: {response}")
+    else:
+        print(f"Doc Bot: {bot_response(user_input)}")
