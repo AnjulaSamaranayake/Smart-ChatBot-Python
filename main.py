b6079ca05a126c9c811afed65e88100154944ebb
AnjulaSamaranayake <anjulasankalpa2255@gmail.com>
2025-03-12 08:22:54 +0530

    Print the corpus

---
+++ main.py
@@ -0,0 +1,25 @@
+#Import the libraries
+from newspaper import Article
+import random
+import string
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
+
+#Download the punkt package
+nltk.download('punkt', quiet=True)
+
+#Get the Article
+article = Article('https://www.mayoclinic.org/diseases-conditions/chronic-kidney-disease/symptoms-causes/syc-20354521')
+article.download()
+article.parse()
+article.nlp()
+corpus = article.text
+
+#print the article text
+print(corpus)
+
